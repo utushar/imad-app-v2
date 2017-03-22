97dae99427f275221c6a75a297e190cbec6847e6
utushar <tusssharupadhyay57@gmail.com>
2017-03-22 13:36:41 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.get('/', function (req, res) {
 
 
 app.get('/article-one', function (req, res) {
-  res.send('yha p aagye hum');
+  res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
 });
 app.get('/article-two', function (req, res) {
   res.send('yha p aagye hum');
