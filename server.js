6c58708b01e2e6dc5fe9513e8b363ee37573ece3
utushar <tusssharupadhyay57@gmail.com>
2017-03-22 13:26:43 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,6 +9,17 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+
+app.get('/article-one', function (req, res) {
+  res.send('yha p aagye hum');
+});
+app.get('/article-two', function (req, res) {
+  res.send('yha p aagye hum');
+});
+app.get('/article-three', function (req, res) {
+  res.send('yha p aagye hum');
+});
+
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
